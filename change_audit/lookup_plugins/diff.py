1bfcb3900f6ee442765f0c965f495174324a941c
Jimmy Conner <cigamit@cacti.net>
2022-09-12 13:20:57 -0500

    Update diff.py

--- change_audit/lookup_plugins/diff.py
+++ change_audit/lookup_plugins/diff.py
@@ -50,6 +50,9 @@ RETURN = """
     description:
       - The diff
 """
+import re
+
+from importlib import import_module
 
 from ansible.errors import AnsibleLookupError
 from ansible.plugins.lookup import LookupBase
@@ -71,7 +74,67 @@ class LookupModule(LookupBase):
         if not valid:
             raise AnsibleLookupError(errors)
         updated_data["wantlist"] = False
-
+       
         
+        diff = FactDiff(self);
+        res = diff.diff();
         
         return res
+
+    
+class FactDiffBase:
+    def __init__(self, task_args, task_vars, debug):
+        self._debug = debug
+        self._task_args = task_args
+        self._task_vars = task_vars
+
+class FactDiff(FactDiffBase):
+    def _check_valid_regexes(self):
+        if self._skip_lines:
+            self._debug("Checking regex in 'split_lines' for validity")
+            for idx, regex in enumerate(self._skip_lines):
+                try:
+                    self._skip_lines[idx] = re.compile(regex)
+                except re.error as exc:
+                    msg = "The regex '{regex}', is not valid. The error was {err}.".format(
+                        regex=regex,
+                        err=str(exc),
+                    )
+                    self._errors.append(msg)
+
+    def _xform(self):
+        if self._skip_lines:
+            if isinstance(self._before, str):
+                self._debug("'before' is a string, splitting lines")
+                self._before = self._before.splitlines()
+            if isinstance(self._after, str):
+                self._debug("'after' is a string, splitting lines")
+                self._after = self._after.splitlines()
+            self._before = [
+                line
+                for line in self._before
+                if not any(regex.match(str(line)) for regex in self._skip_lines)
+            ]
+            self._after = [
+                line
+                for line in self._after
+                if not any(regex.match(str(line)) for regex in self._skip_lines)
+            ]
+        if isinstance(self._before, list):
+            self._debug("'before' is a list, joining with \n")
+            self._before = "\n".join(map(str, self._before)) + "\n"
+        if isinstance(self._after, list):
+            self._debug("'after' is a list, joining with \n")
+            self._after = "\n".join(map(str, self._after)) + "\n"
+
+    def diff(self):
+        self._after = self._task_args["after"]
+        self._before = self._task_args["before"]
+        self._errors = []
+        self._skip_lines = False #self._task_args["plugin"]["vars"].get("skip_lines")
+        self._check_valid_regexes()
+        if self._errors:
+            return {"errors": " ".join(self._errors)}
+        self._xform()
+        diff = CallbackBase()._get_diff({"before": self._before, "after": self._after})
+        return {"diff": diff}
